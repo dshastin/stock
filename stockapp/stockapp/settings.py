a30e991c4c6edf776215a0132e7e43edcf19db87
Dionis <denis.shastin@gmail.com>
2022-08-30 22:02:09 +0300

    init

--- stockapp/stockapp/settings.py
+++ stockapp/stockapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from conf import DJANGO_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*k9dd)6_$)*%&9^890g0&$kq%!b)nlng1=t^xm0+&uhj=%1d-e'
+SECRET_KEY = DJANGO_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
