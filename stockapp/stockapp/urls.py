6afbd20f053e4247b75028053e3272417c52f9d4
Dionis <denis.shastin@gmail.com>
2022-09-17 00:34:40 +0300

    templates reformatted

--- stockapp/stockapp/urls.py
+++ stockapp/stockapp/urls.py
@@ -5,6 +5,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='home'),
-    path('portfolio/', include('portfolio.urls')),
+    path('', include('portfolio.urls')),
 ]
